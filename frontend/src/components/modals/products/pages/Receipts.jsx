e5ccaaedb6402890ec8e9efa0d94b73fd11518ef
work <i@ainoMacBook-Air.local>
2023-05-13 23:12:13 +0900

    update

--- frontend/src/components/modals/products/pages/Receipts.jsx
+++ frontend/src/components/modals/products/pages/Receipts.jsx
@@ -89,18 +89,20 @@ export default function Receipts() {
                   <ListGroup.Item>{currentItem.code.branch}</ListGroup.Item>
                 </ListGroup>
               </Row>
-              <ListGroup horizontal className="mb-2">
-                <ListGroup.Item>製品名</ListGroup.Item>
-                <ListGroup.Item>{currentItem.name}</ListGroup.Item>
-              </ListGroup>
-              <ListGroup horizontal className="mb-2">
-                <ListGroup.Item>容量</ListGroup.Item>
-                <ListGroup.Item>{currentItem.volume}ml</ListGroup.Item>
-              </ListGroup>
-              <ListGroup horizontal className="mb-2">
-                <ListGroup.Item>積載数</ListGroup.Item>
-                <ListGroup.Item>{currentItem.maxLoad} cs</ListGroup.Item>
-              </ListGroup>
+              <Col>
+                <ListGroup horizontal className="mb-2">
+                  <ListGroup.Item>製品名</ListGroup.Item>
+                  <ListGroup.Item>{currentItem.name}</ListGroup.Item>
+                </ListGroup>
+                <ListGroup horizontal className="mb-2">
+                  <ListGroup.Item>容量</ListGroup.Item>
+                  <ListGroup.Item>{currentItem.volume}ml</ListGroup.Item>
+                </ListGroup>
+                <ListGroup horizontal className="mb-2">
+                  <ListGroup.Item>積載数</ListGroup.Item>
+                  <ListGroup.Item>{currentItem.maxLoad} cs</ListGroup.Item>
+                </ListGroup>
+              </Col>
             </Col>
             <Col xs={5}>
               <FormGroup className="mb-2" controlId="quantity">
