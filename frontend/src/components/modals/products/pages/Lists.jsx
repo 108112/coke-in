bff1994f5398960d705c5bb41128b85d92302b30
work <i@ainoMacBook-Air.local>
2023-05-16 13:44:44 +0900

    update

--- frontend/src/components/modals/products/pages/Lists.jsx
+++ frontend/src/components/modals/products/pages/Lists.jsx
@@ -6,13 +6,12 @@ import { Container, Table } from "react-bootstrap";
 import { setCurrentItem } from "../../../../features/listSlice";
 import axios from "axios";
 
-
 export default function Lists() {
   const dispatch = useDispatch();
 
   const items = useSelector((state) => state.list.items);
   const pages = useSelector((state) => state.modal.pages);
-  
+
   const selectItem = async (id) => {
     try {
       const response = await axios.get(`/api/items/${id}/select`);
@@ -28,9 +27,8 @@ export default function Lists() {
       <Table striped bordered hover className="table-sticky">
         <thead>
           <tr>
-            <th>JSコード</th>
+            <th>JS</th>
             <th>枝番</th>
-            <th>商品名</th>
             <th>内容量</th>
             <th>賞味期限</th>
             <th>ロケ</th>
@@ -42,15 +40,13 @@ export default function Lists() {
             return (
               <tr
                 key={item._id}
-                onClick={ () => pages !== "Lists" && selectItem(item._id)}
+                onClick={() => pages !== "Lists" && selectItem(item._id)}
               >
-                <td>{item.product.code.JS}</td>
-                <td>{item.product.code.branch}</td>
-                <td>{item.product.name}</td>
+                <td className="col-1">{item.product.code.JS}</td>
+                <td className="col-1">{item.product.code.branch}</td>
                 <td>{item.product.volume}ml</td>
                 <td>{item.bestBefore}</td>
-                <td>{item.location}</td>
-                <td>{item.quantity}</td>
+                <td className="col-2">{item.quantity}</td>
               </tr>
             );
           })}
