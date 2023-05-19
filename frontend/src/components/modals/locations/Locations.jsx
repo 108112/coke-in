45427f562ed4772853cc2bfc09c1f6a868e933d5
work <i@ainoMacBook-Air.local>
2023-05-19 16:44:16 +0900

    update location

--- frontend/src/components/modals/locations/Locations.jsx
+++ frontend/src/components/modals/locations/Locations.jsx
@@ -1,51 +1,28 @@
 import React from "react";
-import axios from "axios";
+import { Col, Container, Row } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
-
 import News from "./pages/News";
+import { setModalPages } from "../../../features/modalSlice";
 
-import "./Products.css";
-
-import {
-  resetCurrentItem,
-  resetItems,
-  setItems,
-} from "../../../features/listSlice";
-
-import { FiPlus, FiLogIn, FiLogOut, FiDownload, FiList } from "react-icons/fi";
-import { Col, Container, Row, Spinner } from "react-bootstrap";
-import { loading } from "../../../features/modalSlice";
-
-export default function Products() {
+export default function Locations() {
   const dispatch = useDispatch();
+
   const page = useSelector((state) => state.modal.pages);
   const isLoading = useSelector((state) => state.modal.isLoading);
 
   const pages = {
-    News: <News />,
-  };
+    News: <News />
+  }
 
   const togglePages = (page) => {
     dispatch(setModalPages(page));
-  };
+  }
 
-  const handleDataFetch = async (page) => {
-    dispatch(loading());
-    try {
-      const response = await axios.get(`/api/${page}/all`);
-      dispatch(setItems(response.data));
-    } catch (err) {
-      console.log(err);
-    }
-    dispatch(loading());
-  };
-
-  //////handlers//////
   const handleOpenNews = (e) => {
     e.preventDefault();
     togglePages("News");
-  };
 
+  }
   return (
     <Container>
       <Row>
@@ -57,11 +34,11 @@ export default function Products() {
         >
           <button className="menuItem" onClick={handleOpenNews}>
             <FiPlus className="d-block d-sm-none" />
-            <span className="d-none d-sm-block">ロケーションの登録</span>
+            <span className="d-none d-sm-block">新商品の登録</span>
           </button>
-          <button className="menuItem" >
+          <button className="menuItem">
             <FiLogIn className="d-block d-sm-none" />
-            <span className="d-none d-sm-block">セクションの登録</span>
+            <span className="d-none d-sm-block">入庫する</span>
           </button>
           <button className="menuItem">
             <FiDownload className="d-block d-sm-none" />
