f3382f4f7a44e954c8146acaae479df3f8e5527e
work <i@ainoMacBook-Air.local>
2023-05-23 03:34:25 +0900

    update

--- frontend/src/components/modals/products/pages/Shipments.jsx
+++ frontend/src/components/modals/products/pages/Shipments.jsx
@@ -48,76 +48,78 @@ function Shipment() {
     }
     setValidated(true);
   };
-  <Form noValidate validated={validated} onSubmit={handleSubmit}>
-    <Row className="text-center mb-2">
-      <h3 className="fw-bold">製品を出庫する</h3>
-    </Row>
-    <Row>
-      <Col xs={12} md={6}>
-        <ListGroup horizontal className="mb-2">
-          <ListGroup.Item className="px-2">JSコード</ListGroup.Item>
-          <ListGroup.Item className="col-4">
-            {currentItem.product.code.JS}
-          </ListGroup.Item>
-        </ListGroup>
-        <ListGroup horizontal className="mb-2">
-          <ListGroup.Item className="px-2">枝番</ListGroup.Item>
-          <ListGroup.Item className="col-4">
-            {currentItem.product.code.branch}
-          </ListGroup.Item>
-        </ListGroup>
-        <ListGroup horizontal className="mb-2">
-          <ListGroup.Item className="px-2">製品名</ListGroup.Item>
-          <ListGroup.Item className="col-7 text-truncate">
-            {currentItem.product.name}
-          </ListGroup.Item>
-        </ListGroup>
-        <ListGroup horizontal className="mb-2">
-          <ListGroup.Item className="px-2">容量</ListGroup.Item>
-          <ListGroup.Item className="col-4">
-            {currentItem.product.volume}ml
-          </ListGroup.Item>
-        </ListGroup>
-        <ListGroup horizontal>
-          <ListGroup.Item className="px-2">積載数</ListGroup.Item>
-          <ListGroup.Item className="col-4">
-            {currentItem.product.maxLoad} cs
-          </ListGroup.Item>
-        </ListGroup>
-      </Col>
-      <Col xs={12} md={6}>
-        <ListGroup horizontal className="mb-2">
-          <ListGroup.Item className="px-2">賞味期限</ListGroup.Item>
-          <ListGroup.Item className="col-6">
-            {currentItem.bestBefore}
-          </ListGroup.Item>
-        </ListGroup>
-        <ListGroup horizontal className="mb-2">
-          <ListGroup.Item className="px-2">数量</ListGroup.Item>
-          <ListGroup.Item className="col-4">
-            {currentItem.quantity} PL
-          </ListGroup.Item>
-        </ListGroup>
-        <FormGroup as={Col} className="mb-2" controlId="formGridQty">
-          <Form.Label>出庫数</Form.Label>
-          <Form.Control
-            type="text"
-            name="quantity"
-            onChange={handleInputChange}
-            required
-            maxLength={2}
-            placeholder="00"
-          />
-          <Form.Control.Feedback type="invalid">
-            数量を入力してください
-          </Form.Control.Feedback>
-        </FormGroup>
-        <Button type="submit" style={{ width: "40%" }} variant="danger">
-          送信
-        </Button>
-      </Col>
-    </Row>
-  </Form>;
+  return (
+    <Form noValidate validated={validated} onSubmit={handleSubmit}>
+      <Row className="text-center mb-2">
+        <h3 className="fw-bold">製品を出庫する</h3>
+      </Row>
+      <Row>
+        <Col xs={12} md={6}>
+          <ListGroup horizontal className="mb-2">
+            <ListGroup.Item className="px-2">JSコード</ListGroup.Item>
+            <ListGroup.Item className="col-4">
+              {currentItem.product.code.JS}
+            </ListGroup.Item>
+          </ListGroup>
+          <ListGroup horizontal className="mb-2">
+            <ListGroup.Item className="px-2">枝番</ListGroup.Item>
+            <ListGroup.Item className="col-4">
+              {currentItem.product.code.branch}
+            </ListGroup.Item>
+          </ListGroup>
+          <ListGroup horizontal className="mb-2">
+            <ListGroup.Item className="px-2">製品名</ListGroup.Item>
+            <ListGroup.Item className="col-7 text-truncate">
+              {currentItem.product.name}
+            </ListGroup.Item>
+          </ListGroup>
+          <ListGroup horizontal className="mb-2">
+            <ListGroup.Item className="px-2">容量</ListGroup.Item>
+            <ListGroup.Item className="col-4">
+              {currentItem.product.volume}ml
+            </ListGroup.Item>
+          </ListGroup>
+          <ListGroup horizontal>
+            <ListGroup.Item className="px-2">積載数</ListGroup.Item>
+            <ListGroup.Item className="col-4">
+              {currentItem.product.maxLoad} cs
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col xs={12} md={6}>
+          <ListGroup horizontal className="mb-2">
+            <ListGroup.Item className="px-2">賞味期限</ListGroup.Item>
+            <ListGroup.Item className="col-6">
+              {currentItem.bestBefore}
+            </ListGroup.Item>
+          </ListGroup>
+          <ListGroup horizontal className="mb-2">
+            <ListGroup.Item className="px-2">数量</ListGroup.Item>
+            <ListGroup.Item className="col-4">
+              {currentItem.quantity} PL
+            </ListGroup.Item>
+          </ListGroup>
+          <FormGroup as={Col} className="mb-2" controlId="formGridQty">
+            <Form.Label>出庫数</Form.Label>
+            <Form.Control
+              type="text"
+              name="quantity"
+              onChange={handleInputChange}
+              required
+              maxLength={2}
+              placeholder="00"
+            />
+            <Form.Control.Feedback type="invalid">
+              数量を入力してください
+            </Form.Control.Feedback>
+          </FormGroup>
+          <Button type="submit" style={{ width: "40%" }} variant="danger">
+            送信
+          </Button>
+        </Col>
+      </Row>
+    </Form>
+  );
 }
 
 export default function Shipments() {
