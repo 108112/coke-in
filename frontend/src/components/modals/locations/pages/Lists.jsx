0593f302b537d1a1c40e281f0a9fc7d283dc4b65
work <i@ainoMacBook-Air.local>
2023-05-25 01:33:03 +0900

    update

--- frontend/src/components/modals/locations/pages/Lists.jsx
+++ frontend/src/components/modals/locations/pages/Lists.jsx
@@ -1,24 +1,8 @@
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Accordion } from "react-bootstrap";
-import axios from "axios";
-import { setCurrentSection } from "../../../../features/listSlice";
+import React from 'react'
 
 export default function Lists() {
-  const dispatch = useDispatch();
-  const locations = useSelector((state) => state.list.locations);
-  const currentSection = useSelector((state) => state.list.currentSection);
-
-  // const selectSection = async (id) => {
-  //   try {
-  //     const response = await axios.get(`/api/locations/${id}/select`);
-  //     dispatch(setCurrentSection(response.data));
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
-
   return (
     <div>Lists</div>
   )
 }
+
