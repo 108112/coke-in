e51c87eebc68219e81f55022893e62bd75bec037
work <i@ainoMacBook-Air.local>
2023-05-25 01:39:15 +0900

    update

--- frontend/src/components/modals/locations/pages/Lists.jsx
+++ frontend/src/components/modals/locations/pages/Lists.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Accordion } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function Lists() {
+  const dispatch = useDispatch();
+  const locations = useSelector((state) => state.list.locations);
   return (
-    <div>Lists</div>
-  )
+    <Accordion>
+      {locations.map((location) => {
+        <Accordion.Item>
+          <Accordion.Header>{`${location.name.floor}${location.name.area}`}</Accordion.Header>
+          {location.sections.map((section) => {
+            <Accordion.Body>{section.name}</Accordion.Body>;
+          })}
+        </Accordion.Item>;
+      })}
+    </Accordion>
+  );
 }
-
