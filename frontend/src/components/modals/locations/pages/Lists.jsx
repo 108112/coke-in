335940a4a219a23fce2509c1f0ed481ff14bdab9
work <i@ainoMacBook-Air.local>
2023-05-25 00:34:32 +0900

    update

--- frontend/src/components/modals/locations/pages/Lists.jsx
+++ frontend/src/components/modals/locations/pages/Lists.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { Accordion } from "react-bootstrap";
 
 export default function Lists() {
