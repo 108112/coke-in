606457076feca484452ea595b70933734dbf4fce
work <i@ainoMacBook-Air.local>
2023-05-25 01:52:02 +0900

    update

--- frontend/src/components/modals/locations/pages/Lists.jsx
+++ frontend/src/components/modals/locations/pages/Lists.jsx
@@ -3,15 +3,16 @@ import { Accordion } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 
 export default function Lists() {
-  const dispatch = useDispatch();
   const locations = useSelector((state) => state.list.locations);
   return (
     <Accordion>
       {locations.map((location) => {
-        <Accordion.Item eventKey={location._id} key={location._id}>
-          <Accordion.Header>{`${location.name.floor}${location.name.area}`}</Accordion.Header>
-          <Accordion.Body>location</Accordion.Body>
-        </Accordion.Item>
+        return (
+          <Accordion.Item eventKey={location._id} key={location._id}>
+            <Accordion.Header>{`${location.name.floor}${location.name.area}`}</Accordion.Header>
+            <Accordion.Body>location</Accordion.Body>
+          </Accordion.Item>
+        );
       })}
     </Accordion>
   );
