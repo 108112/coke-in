7f4693a6a79df51b19a4f67ac0c0895596a1bdde
work <i@ainoMacBook-Air.local>
2023-05-25 00:33:04 +0900

    update

--- frontend/src/components/modals/locations/pages/Lists.jsx
+++ frontend/src/components/modals/locations/pages/Lists.jsx
@@ -1,22 +1,10 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { setCurrentLocation } from "../../../../features/listSlice";
-import { Accordion, Container } from "react-bootstrap";
-import Search from "../../../search/Search";
+import { Accordion } from "react-bootstrap";
 
 export default function Lists() {
-  const dispatch = useDispatch();
   const locations = useSelector((state) => state.list.locations);
-  const pages = useSelector((state) => state.modal.pages);
 
-  const selectLocation = async (id) => {
-    try {
-      const response = await axios.get(`/api/locations/${id}/select`);
-      dispatch(setCurrentLocation(response.data));
-    } catch (err) {
-      console.log(err);
-    }
-  };
   return (
     <Accordion>
       {locations.map((location) => {
