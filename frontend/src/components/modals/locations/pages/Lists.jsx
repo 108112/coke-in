2d084db378a71564a80f2ae5807a7bb23e1a4468
work <i@ainoMacBook-Air.local>
2023-05-30 16:32:50 +0900

    update

--- frontend/src/components/modals/locations/pages/Lists.jsx
+++ frontend/src/components/modals/locations/pages/Lists.jsx
@@ -1,9 +1,20 @@
 import React from "react";
-import { Accordion, ListGroup, Row } from "react-bootstrap";
+import axios from "axios";
+import { setCurrentSection } from "../../../../features/listSlice";
+import { Accordion } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 
 export default function Lists() {
+  const dispatch = useDispatch();
   const locations = useSelector((state) => state.list.locations);
+
+  const selectSection = async (id) => {
+    e.preventDefault();
+    try {
+      const response = await axios.get(`/api/locations/${id}/select`);
+      dispatch(setCurrentSection(response.data));
+    } catch (err) {}
+  };
   return (
     <Accordion>
       {locations.map((location) => {
@@ -16,7 +27,9 @@ export default function Lists() {
                   return (
                     <Accordion.Item eventKey={section._id} key={section._id}>
                       <Accordion.Header>{section.name}</Accordion.Header>
-                      <Accordion.Body>{section.item ? section.item.name : "空き"}</Accordion.Body>
+                      <Accordion.Body onClick={() => selectSection(section._id)}>
+                        {section.item ? section.item.name : "空き"}
+                      </Accordion.Body>
                     </Accordion.Item>
                   );
                 })}
