e7b378bf4c186de7926a90cf5aaa5fa15056e91a
work <i@ainoMacBook-Air.local>
2023-05-30 16:13:35 +0900

    update

--- frontend/src/components/modals/locations/pages/Lists.jsx
+++ frontend/src/components/modals/locations/pages/Lists.jsx
@@ -14,8 +14,8 @@ export default function Lists() {
               {location.sections.map((section) => {
                 return (
                   <ListGroup key={section._id} horizontal>
-                    <ListGroup.Item as={3}>{section.name}</ListGroup.Item>
-                    <ListGroup.Item as={9}>空き</ListGroup.Item>
+                    <ListGroup.Item>{section.name}</ListGroup.Item>
+                    <ListGroup.Item>空き</ListGroup.Item>
                   </ListGroup>
                 );
               })}
